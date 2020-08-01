2a202fee21e6675a1b9ac83f79a39907e7b465fb
Ahmedsafwat101 <42112466+Ahmedsafwat101@users.noreply.github.com>
2020-08-01 18:12:03 +0200

    new Problem
     Detect Capital

---
+++ Detect Capital.java
@@ -0,0 +1,34 @@
+package com.AhmedSafwat;
+
+public class Solution3 {
+    public boolean detectCapitalUse(String word) {
+
+        if(allCapital(word)) return true;
+        else if(allSmall(word)) return true;
+        else if(onlyFirstCapital(word))return true;
+        return false;
+    }
+
+    private boolean  allCapital(String word){
+        String newWord= word.toUpperCase();
+        if(word.equals(newWord)) return true;
+        return false;
+
+    }
+
+    private boolean allSmall(String word){
+        String newWord= word.toLowerCase();
+        if(word.equals(newWord)) return true;
+        return false;
+    }
+
+    private boolean onlyFirstCapital(String word){
+
+          if(Character.toUpperCase(word.charAt(0))!=word.charAt(0))return false;
+
+          for(int i=1;i<word.length();i++){
+              if(Character.toUpperCase(word.charAt(i))==word.charAt(i)) return false;
+          }
+          return true;
+    }
+}
